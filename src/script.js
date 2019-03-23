dddc8063a8953dffcf13f857d1ddc8758945ab40
hlays <lays.hidani@gmail.com>
2019-03-22 22:37:25 -0300

    add icons e login

---
+++ src/script.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyB9RyK56kqSgV4Mr73m3qKmwy0PcGl2Xkk",
+  authDomain: "serview-2019.firebaseapp.com",
+  databaseURL: "https://serview-2019.firebaseio.com",
+  projectId: "serview-2019",
+  storageBucket: "serview-2019.appspot.com",
+  messagingSenderId: "383862721947"
+};
+firebase.initializeApp(config);
