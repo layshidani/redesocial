d9853f634cfd043bfc6d71ac76cecc5a50879c67
hlays <lays.hidani@gmail.com>
2019-03-28 16:50:42 -0300

    modal: botão enviar funcionando

--- src/js/feed.js
+++ src/js/feed.js
@@ -1,24 +1,27 @@
 // Get a reference to the database service
 var database = firebase.database();
 
-  $('#logout-btn').click(function () {
+$('#logout-btn').click(function () {
 
-    firebase
-      .auth()
-      .signOut()
-      .then(function (result) {
-        console.log(result);
-        window.location.href = "index.html";
-      })
+  firebase
+    .auth()
+    .signOut()
+    .then(function (result) {
+      console.log(result);
+      window.location.href = "index.html";
+    })
+});
+
+$('#post-btn').click(function() {
+  let textInput = $('#comment-text').val();
+  console.log('click');
   
-  $('#post-btn').click(function() {
-    const textInput = $('#post-input').val();
-    if (textInput !== '') {
-      $('#posts').append(`<li class='post-card'>${textInput}</li>`);
-      $('#post-input').val('');
-    } else {
-      alert('O campo de texto não pode estar vazio :/');
-    }
-  })
+  if (textInput !== '') {
+    $('#posts').append(`<li class='post-card'>${textInput}</li>`);
+    $('#comment-text').val('');
+    $(this).dialog('close');
+  } else {
+    alert('O campo de texto não pode estar vazio :/');
+  }
 });
 
