467e7f6b77816284b37d486e4278e8831e3fe0b8
sirleyalmeida <sirleyas90@gmail.com>
2019-03-29 16:39:40 -0300

    database email e post funcionando

--- src/js/feed.js
+++ src/js/feed.js
@@ -1,16 +1,50 @@
 // Get a reference to the database service
-var database = firebase.database();
+const database = firebase.database();
 
-$('#post-btn').click(function () {
-  let textInput = $('#comment-text').val();
-  console.log('click');
+$(document).ready(function () {
+  // Email na base de dados
+  $('#btn-create-user').click(function (event) {
+    event.preventDefault();
 
-  if (textInput !== '') {
-    $('#posts').append(`<li class='post-card'>${textInput}</li>`);
-    $('#comment-text').val('');
-    $(this).dialog('close');
-  } else {
-    alert('O campo de texto não pode estar vazio :/');
-  }
+    const email = $('#input-email').val();
+
+    database.ref('email').push({
+      email: email,
+    })
+
+  })
+  // Mensagens na base de dados
+  $('#post-btn').click(function (event) {
+    event.preventDefault();
+
+    let newPost = $('#comment-text').val();
+
+    database.ref('posts').push({
+      text: newPost,
+    })
+
+    if (newPost !== '') {
+      $('#posts').append(`<li class='post-card'>${newPost}</li>`);
+      console.log('funciona')
+      $('#comment-text').val('');
+      $(this).dialog('close');
+    } else {
+      alert('O campo de texto não pode estar vazio :/');
+    }
+  });
+
 });
 
+
+// $('#post-btn').click(function () {
+//   let textInput = $('#comment-text').val();
+//   console.log('click');
+
+//   if (textInput !== '') {
+//     $('#posts').append(`<li class='post-card'>${textInput}</li>`);
+//     $('#comment-text').val('');
+//     $(this).dialog('close');
+//   } else {
+//     alert('O campo de texto não pode estar vazio :/');
+//   }
+// });
