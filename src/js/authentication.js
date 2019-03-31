859eb63e071359cd9c3e518ede2819bd007554b0
hlays <lays.hidani@gmail.com>
2019-03-31 16:41:56 -0300

    implementada parcialmente função de template de post

--- src/js/authentication.js
+++ src/js/authentication.js
@@ -63,6 +63,8 @@ $(document).ready(function () {
       });
   });
 
+  var user = firebase.auth().currentUser;
+
   $('#logout-btn').click(function () {
     firebase
       .auth()
--- src/js/feed.js
+++ src/js/feed.js
@@ -1,68 +1,89 @@
 window.onload = () => {
   // Get a reference to the database service
   const database = firebase.database();
+  const posts = firebase.database().ref('posts');
 
-  var user = firebase.auth().currentUser;
-  var name, email, photoUrl, uid, emailVerified;
-  
-  if (user != null) {
-    name = user.displayName;
-    email = user.email;
-    photoUrl = user.photoURL;
-    emailVerified = user.emailVerified;
-    uid = user.uid;
+  (function () {
+    posts.once('value').then(snapshot => {
+      console.log(snapshot.val());
+    });
+
+
+  })();
+
+  // data e hora do post
+  function getDate() {
+    let date = new Date();
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    let day = date.getDate();
+
+    let postDate = `${day}/${month}/${year} - ${hour}:${min}`
+    return postDate;
+  };
+
+  let likes = 0;
+
+  const postTemplate = function() {
+    // cabeçaho do post
+    let name = document.createElement('h1');
+    name.innerText = '@user';
+
+    let header = document.createElement('h2');
+    header.innerText = getDate();
+    header.classList.add('post-title');
+
+    // mensagem
+    let text = document.createElement('p');
+    text.innerText = newPost;
+    
+    // editar postagem
+    let editPost = document.createElement('a');
+    editPost.innerText = editar;
+
+    // excluir postagem
+    let deletePost = document.createElement('a');
+    deletePost.innerText = excluir;
+
+    // botão curtir
+    let likeBtn = document.createElement('p');
+    likeBtn.textContent = `<a>curtir</a>`
+
+    // contador de curtidas
+    let counter = document.createElement('span');
+    counter.innerHTML = likes;
+
+    // card de postagem
+    let card = document.createElement('div');
+
+    // inserir informações no card
+    card.appendChild(name);
+    card.appendChild(header);
+    card.appendChild(text);
+    card.appendChild(editPost);
+    card.appendChild(deletePost);
+    card.appendChild(likeBtn);
+    card.appendChild(counter);
   }
 
-  $(document).ready(function () {
-    // Email na base de dados
-    $('#btn-create-user').click(function (event) {
-      event.preventDefault();
-      const email = $('#input-email').val();
-      database.ref('email').push({
-        email: email,
+  // Mensagens na base de dados
+  $('#post-btn').click(function (event) {
+    event.preventDefault();
+    let newPost = $('#comment-text').val();
+
+    if (newPost !== '') {
+      database.ref('posts').push({
+        text: newPost,
+        date: getDate(),
       })
-
-    })
-
-
-    // const timePost = (x) => {
-    //   let date = new Date();
-    //   let hour = date.getHours();
-    //   let min = date.getMinutes();
-    //   let hourPost = hour + ":" + min;
-    //   return hourPost;
-    // };
-
-    // console.log(timePost)
-
-    function e() {
-      let date = new Date();
-      let hour = date.getHours();
-      let min = date.getMinutes();
-      let year = date.getFullYear();
-      let month = date.getMonth();
-      let day = date.getDate();
-    
-      let hourPost = `${day}/${month}/${year} - ${hour}:${min}`
-      return hourPost;
+      $('#posts').append(`<li class='post-card'>${getDate()} <br> >>> ${newPost}</li>`);
+      $('#comment-text').val('');
+    } else {
+      alert('O campo de texto não pode estar vazio :/');
     }
-    const hourPost = e();
-
-    // Mensagens na base de dados
-    $('#post-btn').click(function (event) {
-      event.preventDefault();
-      let newPost = $('#comment-text').val();
-
-      if (newPost !== '') {
-        database.ref('posts').push({
-          text: newPost,
-          hour: hourPost,
-        })
-        $('#posts').append(`<li class='post-card'>${hourPost} <br> >>> ${newPost}</li>`);
-        $('#comment-text').val('');
-      } else {
-        alert('O campo de texto não pode estar vazio :/');
-      }
-    });
   });
+  
 };
+
