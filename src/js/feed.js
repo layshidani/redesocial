c1f2ccad180ef21ca646b5e4daabdf056816226a
hlays <lays.hidani@gmail.com>
2019-03-31 14:43:36 -0300

    add hora no post

--- src/js/feed.js
+++ src/js/feed.js
@@ -1,72 +1,64 @@
 window.onload = () => {
-// Get a reference to the database service
-const database = firebase.database();
+  // Get a reference to the database service
+  const database = firebase.database();
 
-$(document).ready(function () {
-  // Email na base de dados
-  $('#btn-create-user').click(function (event) {
-    event.preventDefault();
+  var user = firebase.auth().currentUser;
+  var name, email, photoUrl, uid, emailVerified;
+  
+  if (user != null) {
+    name = user.displayName;
+    email = user.email;
+    photoUrl = user.photoURL;
+    emailVerified = user.emailVerified;
+    uid = user.uid;
+  }
 
-    const email = $('#input-email').val();
+  $(document).ready(function () {
+    // Email na base de dados
+    $('#btn-create-user').click(function (event) {
+      event.preventDefault();
+      const email = $('#input-email').val();
+      database.ref('email').push({
+        email: email,
+      })
 
-    database.ref('email').push({
-      email: email,
     })
 
-  })
-  // Mensagens na base de dados
-  $('#post-btn').click(function (event) {
-    event.preventDefault();
 
-    let newPost = $('#comment-text').val();
+    // const timePost = (x) => {
+    //   let date = new Date();
+    //   let hour = date.getHours();
+    //   let min = date.getMinutes();
+    //   let hourPost = hour + ":" + min;
+    //   return hourPost;
+    // };
 
-    database.ref('posts').push({
-      text: newPost,
-    })
+    // console.log(timePost)
 
-    if (newPost !== '') {
-      $('#posts').append(`<li class='post-card'>${newPost} ${hourPost}</li>`);
-      $('#comment-text').val('');
-      $(this).dialog('close');
-    } else {
-      alert('O campo de texto não pode estar vazio :/');
+    function e() {
+      let date = new Date();
+      let hour = date.getHours();
+      let min = date.getMinutes();
+      let hourPost = hour + ":" + min;
+      return hourPost;
     }
+    const hourPost = e();
+
+    // Mensagens na base de dados
+    $('#post-btn').click(function (event) {
+      event.preventDefault();
+      let newPost = $('#comment-text').val();
+
+      if (newPost !== '') {
+        database.ref('posts').push({
+          text: newPost,
+          hour: hourPost,
+        })
+        $('#posts').append(`<li class='post-card'>${hourPost} <br> >>> ${newPost}</li>`);
+        $('#comment-text').val('');
+      } else {
+        alert('O campo de texto não pode estar vazio :/');
+      }
+    });
   });
-
-});
-
-// const timePost = (x) => {
-//   let date = new Date();
-//   let hour = date.getHours();
-//   let min = date.getMinutes();
-//   let hourPost = hour + ":" + min;
-//   return hourPost;
-// };
-
-// console.log(timePost)
-
-function e() {
-  let date = new Date();
-  let hour = date.getHours();
-  let min = date.getMinutes();
-  let hourPost = hour + ":" + min;
-  return hourPost;
-}
-const hourPost = e();
-console.log(e())
-
-
-
-
-// $('#post-btn').click(function () {
-//   let textInput = $('#comment-text').val();
-//   console.log('click');
-
-//   if (textInput !== '') {
-//     $('#posts').append(`<li class='post-card'>${textInput}</li>`);
-//     $('#comment-text').val('');
-//     $(this).dialog('close');
-//   } else {
-//     alert('O campo de texto não pode estar vazio :/');
-//   }
-// });
+};
