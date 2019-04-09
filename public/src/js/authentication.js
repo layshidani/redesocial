630d2205c46120065ac7b58021cd153d470c4330
hlays <lays.hidani@gmail.com>
2019-04-09 18:32:02 -0300

    add firebase hoisting

---
+++ public/src/js/authentication.js
@@ -0,0 +1,100 @@
+$(document).ready(function () {
+
+  $('#btn-create-user').click(function () {
+    event.preventDefault();
+    let email = $('#input-email').val();
+    console.log(email);
+    let password = $('#input-password').val();
+    let userName = $('#input-name').val();
+    console.log(userName);
+
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(function (users) {
+        let user = firebase.auth().currentUser;
+        user.updateProfile({
+          displayName: userName,
+          photoURL: '',
+        })
+          .then(() => {
+            window.location.href = "categories_create_user.html";
+          });
+
+
+      })
+      .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // alert(errorMessage);
+        $('#error-msg').text(errorMessage);
+        console.log('errorMessage: ', errorMessage);
+      });
+
+  });
+
+  $('#btnLogin').click(function () {
+    event.preventDefault();
+    let email = $('#input-email').val();
+    let password = $('#input-password').val();
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(function (users) {
+        window.location.href = "feed.html";
+      })
+      .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        $('#error-msg').text(errorMessage);
+      });
+  });
+
+  $('#facebook-btn').click(function () {
+    let provider = new firebase.auth.FacebookAuthProvider();
+    // signIn(provider);
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(function (result) {
+        let token = result.credential.accessToken;
+        let user = result.user;
+        let name = result.user.displayName;
+        window.location.href = "feed.html";
+      }).catch(function (error) {
+        alert('Falha na autenticação');
+      });
+  });
+
+  $('#google-btn').click(function () {
+    let provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(function (result) {
+        console.log(result);
+        let token = result.credential.accessToken;
+        let user = result.user;
+        let name = result.user.displayName;
+        window.location.href = "feed.html";
+      }).catch(function (error) {
+        alert('Falha na autenticação');
+      });
+  });
+
+  var user = firebase.auth().currentUser;
+
+  $('#logout-btn').click(function () {
+    firebase
+      .auth()
+      .signOut()
+      .then(function (result) {
+        window.location.href = "index.html";
+      });
+  });
+
+});
+
+
---
+++ public/src/js/feed.js
@@ -0,0 +1,405 @@
+// sirley:
+
+// feedDatabase.child('posts/privados/').once('value').then(snapshot => { console.log(snapshot.val()) })
+
+
+window.onload = () => {
+  event.preventDefault();
+  const database = firebase.database();
+  const feedDatabase = database.ref('feed');
+  const postsContainer = $('#posts-container')[0];
+  const users = database.ref('users');
+
+  let name, email, photoUrl, uid, emailVerified;
+  firebase.auth().onAuthStateChanged(function (user) {
+    firebase.auth
+    if (user) {
+      // user signed in
+      name = user.displayName;
+      console.log('name: ', name);
+      email = user.email;
+      console.log('email: ', email);
+      photoUrl = user.photoURL;
+      console.log('photoUrl: ', photoUrl);
+      emailVerified = user.emailVerified;
+      console.log('emailVerified: ', emailVerified);
+      uid = user.uid;
+      // padrão mostra todos posts
+      console.log('uid: ', uid);
+      showAllPosts(uid);
+    } else {
+      // No user is signed in.
+    }
+  });
+
+  // feedDatabase.child('posts/privates/').once('value').then(snapshot => {
+  //   console.log(snapshot.val())
+
+  // })
+
+  // mostrar todos os posts
+  // database.ref('feed/posts').once('value').then(snapshot => {
+  //   snapshot.forEach(value => {
+  //     const childkey = value.key;
+  //     const childData = value.val();
+  //     const firebaseDate = childData.date;
+  //     const firebaseLocalName = childData.localName;
+  //     const firebaseLocalAdress = childData.localAdress;
+  //     const firebaseLocalHourFrom = childData.localHourFrom;
+  //     const firebaseLocalHourTo = childData.localHourTo;
+  //     const firebaseLocalPrice = childData.localPrice;
+  //     const firebaseText = childData.text;
+  //     const firebaseLikes = childData.likes;
+  //     const firebaseName = childData.name;
+  //     const firebaseEmail = childData.email;
+  //     const firebasePostType = childData.postType;
+
+  //     postTemplate(firebaseDate, firebaseLocalName, firebaseLocalAdress, firebaseLocalHourFrom, firebaseLocalHourTo, firebaseLocalPrice, firebaseText, firebaseLikes, childkey, firebaseName, firebaseEmail);
+
+  // let filter = $('#filter-posts');
+  // filter.change(function () {
+  //   let filterChoice = filter.val();
+  //   console.log(filterChoice)
+  //   if (filterChoice === 'all') {
+  //     $('#posts-container').empty();
+  //     postTemplate(firebaseDate, firebaseLocalName, firebaseLocalAdress, firebaseLocalHourFrom, firebaseLocalHourTo, firebaseLocalPrice, firebaseText, firebaseLikes, childkey, firebaseName, firebaseEmail);
+  //   } else if (filterChoice === 'private') {
+  //     $('#posts-container').empty();
+  //     if (firebasePostType === 'postPrivate') {
+  //       feedDatabase.child('posts/privates/').once('value').then(snapshot => {
+  //         console.log(snapshot.val())
+  //         let userPrivatePost = snapshot.val();
+
+  //       })
+  //       postTemplate(firebaseDate, firebaseLocalName, firebaseLocalAdress, firebaseLocalHourFrom, firebaseLocalHourTo, firebaseLocalPrice, firebaseText, firebaseLikes, childkey, firebaseName, firebaseEmail);
+  //     }
+  //   } else if (filterChoice === 'public') {
+  //     $('#posts-container').empty();
+  //     if (firebasePostType === 'postPublic') {
+  //       postTemplate(firebaseDate, firebaseLocalName, firebaseLocalAdress, firebaseLocalHourFrom, firebaseLocalHourTo, firebaseLocalPrice, firebaseText, firebaseLikes, childkey, firebaseName, firebaseEmail);
+  //     }
+  //   }
+  // })
+  // $('#filter-posts').change(function () {
+  //   if ($('#filter-posts').val() === 'all') {
+  //     $('#posts-container').empty();
+  //     postTemplate(firebaseDate, firebaseLocalName, firebaseLocalAdress, firebaseLocalHourFrom, firebaseLocalHourTo, firebaseLocalPrice, firebaseText, firebaseLikes, childkey, firebaseName, firebaseEmail);
+  //   } else if ($('#filter-posts').val() === 'private') {
+  //     $('#posts-container').empty();
+  //     if (firebasePostType === 'postPrivate') {
+  //       postTemplate(firebaseDate, firebaseLocalName, firebaseLocalAdress, firebaseLocalHourFrom, firebaseLocalHourTo, firebaseLocalPrice, firebaseText, firebaseLikes, childkey, firebaseName, firebaseEmail);
+  //     }
+  //   } else if ($('#filter-posts').val() === 'public') {
+  //     $('#posts-container').empty();
+  //     if (firebasePostType === 'postPublic') {
+  //       postTemplate(firebaseDate, firebaseLocalName, firebaseLocalAdress, firebaseLocalHourFrom, firebaseLocalHourTo, firebaseLocalPrice, firebaseText, firebaseLikes, childkey, firebaseName, firebaseEmail);
+  //     }
+  //   }
+  // })
+  // })
+
+  // mostra posts filtrados
+
+  let filter = $('#filter-posts');
+  filter.change(function () {
+    let filterChoice = filter.val();
+    console.log('filterChoice: ', filterChoice);
+    $('#posts-container').empty();
+
+    if (filterChoice === 'all') {
+      showAllPosts(uid);
+    }
+    showPostsFiltered(filterChoice, uid)
+  })
+
+
+  function showAllPosts(uid) {
+    database.ref('feed/posts').once('value').then(snapshot => {
+      snapshot.forEach(value => {
+        var childkey = value.key;
+        var childData = value.val();
+        var firebasePostType = childData.postType;
+
+        if (childData.uid === uid) {
+          var firebaseDate = childData.date;
+          var firebaseLocalName = childData.localName;
+          var firebaseLocalAdress = childData.localAdress;
+          var firebaseLocalHourFrom = childData.localHourFrom;
+          var firebaseLocalHourTo = childData.localHourTo;
+          var firebaseLocalPrice = childData.localPrice;
+          var firebaseText = childData.text;
+          var firebaseLikes = childData.likes;
+          var firebaseName = childData.name;
+          var firebaseEmail = childData.email;
+          // 
+          postTemplate(firebaseDate, firebaseLocalName, firebaseLocalAdress, firebaseLocalHourFrom, firebaseLocalHourTo, firebaseLocalPrice, firebaseText, firebaseLikes, childkey, firebaseName, firebaseEmail, firebasePostType, firebasePostType);
+        }
+      })
+    })
+
+  }
+
+  function showPostsFiltered(type, uid) {
+    database.ref('feed/posts').once('value').then(snapshot => {
+      snapshot.forEach(value => {
+        var childkey = value.key;
+        var childData = value.val();
+        var firebasePostType = childData.postType;
+
+        if (firebasePostType === type && childData.uid === uid) {
+          var firebaseDate = childData.date;
+          var firebaseLocalName = childData.localName;
+          var firebaseLocalAdress = childData.localAdress;
+          var firebaseLocalHourFrom = childData.localHourFrom;
+          var firebaseLocalHourTo = childData.localHourTo;
+          var firebaseLocalPrice = childData.localPrice;
+          var firebaseText = childData.text;
+          var firebaseLikes = childData.likes;
+          var firebaseName = childData.name;
+          var firebaseEmail = childData.email;
+          // 
+          postTemplate(firebaseDate, firebaseLocalName, firebaseLocalAdress, firebaseLocalHourFrom, firebaseLocalHourTo, firebaseLocalPrice, firebaseText, firebaseLikes, childkey, firebaseName, firebaseEmail, firebasePostType, firebasePostType);
+        }
+      })
+    })
+
+  }
+
+  // data e hora do post
+  function getDate() {
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    let day = date.getDate();
+    let time = Date().split(' ')[4];
+
+    let postDate = `${day}/${month}/${year} - ${time}`
+    return postDate;
+  };
+
+  // template dos posts
+  const postTemplate = function (date, local, address, hourFrom, hourTo, price, textPost, likes, key, userName, userEmail, typeChoose) {
+    // cabeçaho do post
+    let name = document.createElement('p');
+    name.setAttribute('class', 'user-name');
+    name.innerHTML = `${userName} - ${userEmail}`;
+
+
+    let header = document.createElement('span');
+    header.setAttribute('class', 'date-time');
+    header.innerText = date;
+
+    // info local
+    let localInfo = document.createElement('p');
+    localInfo.setAttribute('id', 'local-info');
+    localInfo.setAttribute('class', 'local-info');
+    localInfo.setAttribute('info-data-id', key);
+    localInfo.innerHTML = `<i class="fas fa-map-marker-alt"></i><span class="bold-text">${local}</span> - ${address}`;
+
+    // info horário de funcionamento
+    let operatingHours = document.createElement('p');
+    operatingHours.setAttribute('id', 'local-operating');
+    operatingHours.setAttribute('class', 'local-info');
+    operatingHours.setAttribute('hour-data-id', key);
+    operatingHours.innerHTML = `<i class="fas fa-history"></i>Horário de funcionamento: ${hourFrom}h às ${hourTo}h`;
+
+    // info preço médio
+    let localPrice = document.createElement('p');
+    localPrice.setAttribute('id', 'local-price');
+    localPrice.setAttribute('class', 'local-info');
+    localPrice.setAttribute('price-data-id', key);
+    localPrice.innerHTML = `<i class="fas fa-hand-holding-usd"></i>${price},00`
+
+
+    // mensagem
+    let text = document.createElement('p');
+    text.setAttribute('class', 'text-post');
+    text.setAttribute('id', 'comment-post');
+    text.setAttribute('text-data-id', key);
+    text.innerHTML = textPost;
+
+    // editar postagem
+    let editPost = document.createElement('button');
+    editPost.setAttribute('class', 'post-btn');
+    editPost.setAttribute('id', 'edit-btn');
+    editPost.setAttribute('class', 'far fa-edit btn btn-default navbar-btn');
+    editPost.setAttribute('edit-data-id', key);
+    editPost.setAttribute('data-target', '#newModal');
+    editPost.setAttribute('data-toggle', 'modal');
+    editPost.innerText = '';
+
+    // excluir postagem
+    let deletePost = document.createElement('button');
+    deletePost.setAttribute('class', 'post-btn');
+    deletePost.setAttribute('id', 'delete-btn');
+    deletePost.setAttribute('data-id', key);
+    deletePost.setAttribute('class', 'far fa-trash-alt btn btn-default navbar-btn');
+    deletePost.innerText = '';
+
+    // botão curtir
+    let likeBtn = document.createElement('button');
+    likeBtn.setAttribute('id', 'like-btn');
+    likeBtn.setAttribute('like-data-id', key);
+    likeBtn.setAttribute('class', 'far fa-thumbs-up btn btn-default navbar-btn');
+    likeBtn.innerText = '';
+
+    // contador de curtidas
+    let counter = document.createElement('span');
+    counter.setAttribute('id', 'show-likes');
+    counter.setAttribute('class', 'show-likes');
+    counter.setAttribute('counter-data-id', key);
+    counter.innerHTML = likes + ' curtidas';
+
+    // público ou privado
+    let showSelected = document.createElement('p');
+    showSelected.setAttribute('selected-data-id', key);
+    showSelected.setAttribute('id', 'show-selected');
+
+    showSelected.innerHTML = `${typeChoose}`;
+
+
+    // card de postagem
+    let card = document.createElement('div');
+    card.setAttribute('class', 'post-card');
+    card.setAttribute('id', 'post-card-key');
+    card.setAttribute('data-idcard', key);
+
+    // linha horizontal
+    let headerLine = document.createElement('hr');
+    let footerLine = document.createElement('hr');
+
+    // inserir informações no card
+    card.appendChild(name);
+    card.appendChild(header);
+    card.appendChild(localInfo);
+    card.appendChild(operatingHours);
+    card.appendChild(localPrice);
+    card.appendChild(headerLine);
+    card.appendChild(text);
+    card.appendChild(footerLine);
+    card.appendChild(editPost);
+    card.appendChild(deletePost);
+    card.appendChild(likeBtn);
+    card.appendChild(counter);
+    card.appendChild(showSelected);
+
+    // adiciona card no container de posts
+    postsContainer.insertBefore(card, postsContainer.childNodes[0]);
+  }
+
+  // desabilita postagem caso campo vazio
+  $('#comment-text').keyup(function desablePost() {
+    if ($('#comment-text').val().length > 0) {
+      $('#post-btn').prop("disabled", false);
+    } else {
+      $('#post-btn').prop("disabled", true)
+    }
+  });
+
+  // publicar post
+  $('#post-btn').click(function publishPost() {
+    let inputLocalName = $('#local-name').val();
+    let inputLocalAdress = $('#adress').val();
+    let inputLocalHourFrom = $('#hour-from').val();
+    let inputLocalHourTo = $('#hour-to').val();
+    let inputLocalPrice = $('#average-price').val();
+    let inputText = $('#comment-text').val();
+    let typeSelected = $('#select-post-type').val();
+    let likeInit = 0;
+
+    const newPost = {
+      name: name,
+      uid: uid,
+      email: email,
+      date: getDate(),
+      localName: inputLocalName,
+      localAdress: inputLocalAdress,
+      localHourFrom: inputLocalHourFrom,
+      localHourTo: inputLocalHourTo,
+      localPrice: inputLocalPrice,
+      text: inputText,
+      likes: likeInit,
+      postType: typeSelected,
+    }
+
+    $('#filter-posts').val('all');
+
+    feedDatabase.child('/posts/').push(newPost).then((snapshot) => postTemplate(getDate(), inputLocalName, inputLocalAdress, inputLocalHourFrom, inputLocalHourTo, inputLocalPrice, inputText, likeInit, snapshot.key, name, email, typeSelected));
+
+    // if ($('#select-post-type').val() === 'postPublic') {
+    //   feedDatabase.child('/posts/' + 'publics/' + uid).push(newPost).then((snapshot) => postTemplate(getDate(), inputLocalName, inputLocalAdress, inputLocalHourFrom, inputLocalHourTo, inputLocalPrice, inputText, likeInit, snapshot.key, name, email));
+    // } else {
+    //   feedDatabase.child('/posts/' + 'privates/' + uid).push(newPost).then((snapshot) => postTemplate(getDate(), inputLocalName, inputLocalAdress, inputLocalHourFrom, inputLocalHourTo, inputLocalPrice, inputText, likeInit, snapshot.key, name, email));
+    // }
+  });
+
+  // deletar post
+  $(document).on('click', '#delete-btn', function () {
+    let confirmDelete = confirm('Tem certeza que quer excluir?');
+    if (confirmDelete) {
+      let cardKey = $(this).attr('data-id');
+      feedDatabase.child('/posts/' + cardKey).remove().then(() => {
+        $(this).parent('.post-card').remove();
+      });
+    }
+  })
+
+  // curtidas
+  $(document).on('click', '#like-btn', function () {
+    let likeId = $(this).attr('like-data-id');
+    console.log(likeId)
+    let countLikes = parseInt($(`span[counter-data-id="${likeId}"`).text());
+    console.log(countLikes)
+    countLikes++;
+    feedDatabase.child('posts/' + likeId + '/likes').set(countLikes).then(() => {
+      $(`span[counter-data-id='${likeId}'`).text(`${countLikes} curtidas`);
+    })
+  });
+
+  $('#new-comment-text').keyup(function () {
+    if ($('#new-comment-text').val().length > 0) {
+      console.log($('#new-comment-text').val().length);
+      $('#new-post-btn').prop("disabled", false);
+    } else {
+      $('#new-post-btn').prop("disabled", true);
+    }
+  });
+
+  // editar post
+  $(document).on('click', '#edit-btn', function () {
+    let editKey = $(this).attr('edit-data-id');
+
+    let oldLocalinfo = $(`p[info-data-id=${editKey}]`).text();
+    let oldOperating = $(`p[hour-data-id=${editKey}]`).text();
+    let oldAveragePrice = $(`p[price-data-id=${editKey}]`).text();
+    let oldText = $(`p[text-data-id=${editKey}]`).text();
+
+    $('#new-local-name').val(oldLocalinfo);
+    // $('#new-adress').val(oldLocalinfo);
+    $('#new-hour-from').val(oldOperating);
+    $('#new-price').val(oldAveragePrice);
+    $('#new-comment-text').val(oldText);
+
+    $('#new-post-btn').click(function () {
+      let newName = $('#new-local-name').val();
+      let newAdress = $('#new-adress').val();
+      let newOperating = $('#new-hour-from').val();
+      let newPrice = $('#new-price').val();
+      let newText = $('#new-comment-text').val();
+      let newType = $('#select-new-post-type').val();
+
+      feedDatabase.child('/posts/' + editKey).update({
+        localName: newName,
+        localAdress: newAdress,
+        localHourFrom: newOperating,
+        // localHourTo: newHourTo,
+        localPrice: newPrice,
+        text: `${newText}<span class='edited'>(editado)</span>`,
+        likes: parseInt($(`span[counter-data-id="${editKey}"`).text()),
+        postType: newType,
+      }).then(() => {
+        location.reload();
+      })
+    })
+  });
+};
